1ab132132778600d0a3d95b622b95a6b5fde6d67
Pranavpatell <89290846+Pranavpatell@users.noreply.github.com>
2025-04-15 09:17:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const mongoose = require('mongoose');
-const express = require('express');
 const path = require('path');
-const app = express();
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));

73d5d4fc6dd918fb1519c10f401383f6f55e3abb
Pranavpatell <89290846+Pranavpatell@users.noreply.github.com>
2025-04-15 09:01:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-// const express = require('express');
-// const app = express();
-const path = require('path');
-
-// Serve static files (like index.html)
-// app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(3000, () => {
-  console.log('Server started at http://localhost:3000');
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World, from Node.js!');
-});
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -52,6 +34,9 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('User', userSchema);
 
+app.get('/', (req, res) => {
+  res.send('Hello World, from Node.js!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
